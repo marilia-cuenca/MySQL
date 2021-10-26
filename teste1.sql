f637b51c6ffcc3d1183166736bba24d73db4a4e5
Marília Cuenca <82479792+marilia-cuenca@users.noreply.github.com>
2021-10-26 17:28:19 -0300

    Update teste1.sql

--- teste1.sql
+++ teste1.sql
@@ -5,11 +5,11 @@ default collate utf8_general_ci;
 
 create table pessoas (
 	id int not null auto_increment,
-    nome varchar(30) not null,
+	nome varchar(30) not null,
 	nascimento date,
-    sexo enum('M', 'F'),
+	sexo enum('M', 'F'),
 	peso decimal(5,2),
-    altura decimal(3,2),
-    nacionalidade varchar(20) default 'Brasil',
-    primary key (id)    
+	altura decimal(3,2),
+	nacionalidade varchar(20) default 'Brasil',
+	primary key (id)    
 ) default charset = utf8;
